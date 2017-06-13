ee6959cee6fe268f785aab74c4dd2beeea7d3858
rista404 <risticn6@gmail.com>
2017-06-13 14:25:46 +0200

    Add arduino c code.

--- app.js
+++ app.js
@@ -12,15 +12,8 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html')
 })
 
-const random = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
-
 io.on('connection', (socket) => {
 	console.log('Socket connected.')
-	// socket.emit('news', { hello: 'world' })
-	//
-	// setInterval(() => {
-	// 	socket.emit('temp:in', random(-12, 45))
-	// }, 5000)
 
 	socket.on('temp:server', (data) => {
 		console.log(data)
